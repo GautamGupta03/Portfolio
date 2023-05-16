49e1599a4447f9e100be50222a94a7aa27a3c1e8
GautamGupta03 <0808CI191045.ies@ipsacademy.org>
2023-05-16 06:15:44 +0530

    Third Commit

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Backend developer</h3>
+
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                <i class='bx bx-badge-check' ></i>
+
+                <div>
+                    <h3 className="skills__name">JAVA</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i class='bx bx-badge-check' ></i>
+
+                <div>
+                    <h3 className="skills__name">MySQL</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i class='bx bx-badge-check' ></i>
+
+                <div>
+                    <h3 className="skills__name">PHP</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i class='bx bx-badge-check' ></i>
+
+                <div>
+                    <h3 className="skills__name">Spring/SpringBoot</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Backend
