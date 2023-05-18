1d4dca59e6662f5eed3f9b95f1bff3d7de5f7223
GautamGupta03 <0808CI191045.ies@ipsacademy.org>
2023-05-18 23:19:50 +0530

    fifth commit

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -4,7 +4,6 @@ const Frontend = () => {
   return (
     <div className="skills__content">
         <h3 className="skills__title">Frontend developer</h3>
-
         <div className="skills__box">
             <div className="skills__group">
                 <div className="skills__data">
